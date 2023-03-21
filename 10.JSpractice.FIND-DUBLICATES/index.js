630224eefd3c9f2b18001ac3b41ef20f981366c6
nikoloz kakabadze <nk360dk@gmail.com>
2023-03-21 07:08:22 -0700

    practice

--- 10.JSpractice.FIND-DUBLICATES/index.js
+++ 10.JSpractice.FIND-DUBLICATES/index.js
@@ -1,9 +1,6 @@
 const firstarray = [1, 2, -2, 4, 5, 4, 7, 8, 7, 7, 71, 3, 6];
 function uniqArrayi(arr) {
-  const setone = new Set();
-  for (let elem of arr) {
-    setone.add(elem);
-  }
+  const setone = new Set([...arr]);
   return [...setone];
 }
 console.log(uniqArrayi(firstarray));
--- 11.JSpratice.REMOVE-DUBLICATES/index.js
+++ 11.JSpratice.REMOVE-DUBLICATES/index.js
@@ -1,10 +1,9 @@
 const cararray = ["bmw", "opel", "lada", "opel", "BMW"];
 function uniqArrayi(arr) {
-  const setone = new Set();
   for (let elem of arr) {
     elem = elem.toLowerCase();
-    setone.add(elem);
   }
+  const setone = new Set([arr]);
   return [...setone];
 }
 console.log(uniqArrayi(cararray));

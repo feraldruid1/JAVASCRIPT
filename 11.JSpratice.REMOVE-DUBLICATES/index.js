603651cefe7fc11e95e44728146afd71c154a978
nikoloz kakabadze <nk360dk@gmail.com>
2023-03-21 07:28:41 -0700

    practice

--- 11.JSpratice.REMOVE-DUBLICATES/index.js
+++ 11.JSpratice.REMOVE-DUBLICATES/index.js
@@ -1,9 +1,10 @@
 const cararray = ["bmw", "opel", "lada", "opel", "BMW"];
 function uniqArrayi(arr) {
+  const setone = new Set();
   for (let elem of arr) {
     elem = elem.toLowerCase();
+    setone.add(elem);
   }
-  const setone = new Set([arr]);
   return [...setone];
 }
 console.log(uniqArrayi(cararray));

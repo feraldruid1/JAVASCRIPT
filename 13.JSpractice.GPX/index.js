738d2dfba8203ce660d412ba7c80dc124815b00d
nikoloz kakabadze <nk360dk@gmail.com>
2023-03-21 22:27:50 -0700

    practice

---
+++ 13.JSpractice.GPX/index.js
@@ -0,0 +1,14 @@
+const arr = ["G", "P", "X"];
+console.log(fillArr(arr, 10));
+function fillArr(array, index) {
+  const newarray = [];
+  let count = 0;
+  for (let i = 0; i < index; i++) {
+    newarray.push(array[count]);
+    count++;
+    if (array[count] === undefined) {
+      count = 0;
+    }
+  }
+  return newarray;
+}

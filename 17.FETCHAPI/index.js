c9e1d16b434227ca2ca08a17be30a3781318592c
nikoloz kakabadze <nk360dk@gmail.com>
2023-04-10 10:43:51 -0700

    commit

---
+++ 17.FETCHAPI/index.js
@@ -0,0 +1,6 @@
+const html = document.querySelector("body");
+fetch(`https://api.github.com/users/otarza`)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+const div = `<div>${data.followers}<div>`;
+html.insertAdjacentHTML("beforeend", div);

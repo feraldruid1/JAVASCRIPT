297e09740cc5d9c99f07b0447ed0738da6d719c3
nikoloz kakabadze <nk360dk@gmail.com>
2023-04-10 12:44:06 -0700

    commit

--- 17.FETCHAPI/index.js
+++ 17.FETCHAPI/index.js
@@ -1,8 +1,19 @@
-const html = document.querySelector("body");
-fetch(`https://api.github.com/users/otarza`)
-  .then((response) => response.json())
-  .then((data) => {
-    return console.log(data);
-  });
-const div = `<div>${data.followers}<div>`;
-html.insertAdjacentHTML("beforeend", div);
+const body = document.querySelector("body");
+const input = document.querySelector("input");
+const main = document.querySelector("main");
+function github() {
+  fetch(`https://api.github.com/users/${input.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      main.innerHTML = `<main> <div>${data.followers} <span>followers</span></div><div>${data.public_repos}<span>repos</span></div> <div>${data.following} <span>following</span></div></main>`;
+      main.style = "margin-top; 30px; width: 326px; height: 306px; background-color: white;border-radius: 15px;display: flex;justify-content: space-around;align-items: center;";
+    });
+}
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    github();
+  }
+  if (event.key != "Enter") {
+    return;
+  }
+});

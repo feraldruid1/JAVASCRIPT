5a751cb4ba26317911d9edb941ee741e47bab5eb
nikoloz kakabadze <nk360dk@gmail.com>
2023-04-11 11:19:33 -0700

    practice

--- 17.FETCHAPI/index.js
+++ 17.FETCHAPI/index.js
@@ -12,8 +12,6 @@ function github() {
     .then((data) => {
       main.innerHTML = `<img src="${data.avatar_url} alt"">   <div class="login">${data.login}</div><div class="location">${data.location}</div> <div class="borderdiv"> </div> <div class="maindiv"><div class="followers">${data.followers} <span>followers</span></div><div class="repos">${data.public_repos}<span>repos</span></div> <div class="following">${data.following} <span>following</span></div></div>`;
       main.style.display = "flex";
-      console.log(data);
-      console.log(data.followers, data.public_repos, data.following, data.location, data.login);
     });
 }
 input.addEventListener("keypress", function (event) {

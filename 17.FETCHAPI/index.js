ce4d152a35578a50a1f40500bdbd45f9fc0a13bc
nikoloz kakabadze <nk360dk@gmail.com>
2023-04-11 02:39:22 -0700

    practice

--- 17.FETCHAPI/index.js
+++ 17.FETCHAPI/index.js
@@ -5,15 +5,8 @@ function github() {
   fetch(`https://api.github.com/users/${input.value}`)
     .then((response) => response.json())
     .then((data) => {
-      main.innerHTML = `<main> <div>${data.followers} <span>followers</span></div><div>${data.public_repos}<span>repos</span></div> <div>${data.following} <span>following</span></div></main>`;
-      main.style = "margin-top; 30px; width: 326px; height: 306px; background-color: white;border-radius: 15px;display: flex;justify-content: space-around;align-items: center;";
+      main.innerHTML = `<div>${data.followers} <span>followers</span></div><div>${data.public_repos}<span>repos</span></div> <div>${data.following} <span>following</span></div>`;
+      main.style.display = "flex";
     });
 }
-input.addEventListener("keypress", function (event) {
-  if (event.key === "Enter") {
-    github();
-  }
-  if (event.key != "Enter") {
-    return;
-  }
-});
+input.addEventListener("submit", github);

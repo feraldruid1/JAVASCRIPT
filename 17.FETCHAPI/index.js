3f979b30aa66d10b57023fafdba8e5c25eaa5925
nikoloz kakabadze <nk360dk@gmail.com>
2023-04-18 14:43:45 -0700

    practice

--- 17.FETCHAPI/index.js
+++ 17.FETCHAPI/index.js
@@ -1,12 +1,13 @@
 const body = document.querySelector("body");
 const input = document.querySelector("input");
 const form = document.querySelector("form");
+const div = document.querySelector("div");
 function github() {
   fetch(`https://api.github.com/users/${input.value}`)
     .then((response) => {
       if (!response.ok) {
         const status404 = `<main><img style="background-color:white;" src="./images/nouser.png" alt"">   <div class="login">No profile with this username</div> <div class="borderdiv"> </div> <div class="maindiv"><div class="followers">-- <span>followers</span></div><div class="repos">--<span>repos</span></div> <div class="following">-- <span>following</span></div></div></main>`;
-        body.insertAdjacentHTML("beforeend", status404);
+        div.innerHTML = status404;
         input.value = "";
         return console.log("profile not found");
       }
@@ -14,7 +15,7 @@ function github() {
     })
     .then((data) => {
       const status200 = `<main><img src="${data.avatar_url}" alt"">   <div class="login">${data.login}</div><div class="location">${data.location}</div> <div class="borderdiv"> </div> <div class="maindiv"><div class="followers">${data.followers} <span>followers</span></div><div class="repos">${data.public_repos}<span>repos</span></div> <div class="following">${data.following} <span>following</span></div></div></main>`;
-      body.insertAdjacentHTML("beforeend", status200);
+      div.innerHTML = status200;
       input.value = "";
     });
 }

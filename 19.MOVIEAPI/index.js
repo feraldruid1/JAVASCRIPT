22eeaf213fd381b98d875e1c1178529c33965a56
nikoloz kakabadze <nk360dk@gmail.com>
2023-04-26 10:44:48 -0700

    push

--- 19.MOVIEAPI/index.js
+++ 19.MOVIEAPI/index.js
@@ -1,8 +1,7 @@
 const input = document.querySelector("input");
 const main = document.querySelector("main");
 const form = document.querySelector("form");
-const datagenres = [];
-const filmgenre = [];
+let genres = [];
 const apikey = "ca13458cc8f7d31e877d4c3e5247aac9";
 function fetchmovie() {
   fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&language=en-US&page=1`)
@@ -10,17 +9,10 @@ function fetchmovie() {
       return response.json();
     })
     .then((data) => {
-      apigenres();
-      console.log(data);
-
       for (let k = 0; k < data.results.length; k++) {
-        for (y = 0; y < data.results[k].genre_ids.length; y++) {
-          if (data.results[k].genre_ids[y] === datagenres[k].id) {
-            filmgenre.push(datagenres[k].name);
-          }
-        }
-        div = `<div class="conteiner"><img src="http://image.tmdb.org/t/p/w500/${data.results[k].poster_path}"><div class="infodiv"><h1>${data.results[k].original_title}</h1><span class="genre">${"moviegenre"}</span><div class="stars"></div> <div class="enddiv"><span class="date">${data.results[k].release_date.slice(0, 4)}</span><span>${data.results[k].vote_average}</span> </div> </div>`;
+        div = `<div class="conteiner"><img src="http://image.tmdb.org/t/p/w500/${data.results[k].poster_path}"><div class="infodiv"><h1>${data.results[k].original_title}</h1><span class="genre">${"moviegenre"}</span><div class="stars">${svg}</div> <div class="enddiv"><span class="date">${data.results[k].release_date.slice(0, 4)}</span><span>${data.results[k].vote_average}</span> </div> </div>`;
         main.insertAdjacentHTML("beforeend", div);
+        svg = "";
       }
     });
 }
@@ -29,12 +21,13 @@ form.addEventListener("submit", function (event) {
   event.preventDefault();
 });
 
-function apigenres() {
+(function apigenres() {
   fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${apikey}&language=en-US`)
     .then((response) => response.json())
     .then((data) => {
       for (i = 0; i < data.genres.length; i++) {
-        datagenres.push(data.genres[i]);
+        genres.push(data.genres[i]);
       }
+      console.log(genres);
     });
-}
+})();

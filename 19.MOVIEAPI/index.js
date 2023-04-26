72692581039f9ad8a4b3537f1ed0ed97846f4db8
nikoloz kakabadze <nk360dk@gmail.com>
2023-04-26 11:51:05 -0700

    practice

--- 19.MOVIEAPI/index.js
+++ 19.MOVIEAPI/index.js
@@ -2,6 +2,7 @@
 const input = document.querySelector("input");
 const main = document.querySelector("main");
 const form = document.querySelector("form");
+let img = `<img class="star" src="./images/star.svg" alt="star" />`;
 let genres = [];
 const apikey = "ca13458cc8f7d31e877d4c3e5247aac9";
 moviedb(`https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&language=en-US&page=1`);
@@ -28,13 +29,18 @@ function moviedb(link) {
     .then((data) => {
       for (let k = 0; k < data.results.length; k++) {
         if (data.results[k].poster_path === null) {
-          const div = `<div class="conteiner"><div class="IMG"><h1>No Poster</h1></div><div class="infodiv"><div class="infodiv"><h1>${data.results[k].original_title}</h1><span class="genre">${"moviegenre"}</span> <div class="enddiv"> <span class="date">${data.results[k].release_date.slice(0, 4)}</span><span>${data.results[k].vote_average}</span> </div> </div></div>`;
+          const div = `<div class="conteiner"><div class="IMG"><h1>No Poster</h1></div><div class="infodiv"><div class="infodiv"><h1>${data.results[k].original_title}</h1><span class="genre">${"moviegenre"}</span>${img}<div class="enddiv"> <span class="date">${data.results[k].release_date.slice(0, 4)}</span><span>${data.results[k].vote_average}</span> </div> </div></div>`;
           main.insertAdjacentHTML("beforeend", div);
         } else {
-          const div = `<div class="conteiner"><img src="http://image.tmdb.org/t/p/w500/${data.results[k].poster_path}"><div class="infodiv"><div class="infodiv"><h1>${data.results[k].original_title}</h1><span class="genre">${"moviegenre"}</span><div class="stars"></div> <div class="enddiv"> <span class="date">${data.results[k].release_date.slice(0, 4)}</span><span>${data.results[k].vote_average}</span> </div> </div></div>`;
+          const div = `<div class="conteiner"><img src="http://image.tmdb.org/t/p/w500/${data.results[k].poster_path}"><div class="infodiv"><div class="infodiv"><h1>${data.results[k].original_title}</h1><span class="genre">${"moviegenre"}</span>${img} <div class="enddiv"> <span class="date">${data.results[k].release_date.slice(0, 4)}</span><span>${data.results[k].vote_average}</span> </div> </div></div>`;
           main.insertAdjacentHTML("beforeend", div);
         }
       }
     });
   main.innerHTML = "";
+}
+function starimg(number) {
+  for (let i = 0; i < number; i++) {
+    let img = `<img class="star" src="./images/star.svg" alt="star" />`;
+  }
 }

6ed0d63b85128b17bec268c325203025780b6f73
nikoloz kakabadze <nk360dk@gmail.com>
2023-04-26 12:03:17 -0700

    push

--- 19.MOVIEAPI/index.js
+++ 19.MOVIEAPI/index.js
@@ -2,7 +2,6 @@
 const input = document.querySelector("input");
 const main = document.querySelector("main");
 const form = document.querySelector("form");
-let img;
 let genres = [];
 const apikey = "ca13458cc8f7d31e877d4c3e5247aac9";
 moviedb(`https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&language=en-US&page=1`);
@@ -18,7 +17,6 @@ form.addEventListener("submit", function (event) {
       for (let i = 0; i < data.genres.length; i++) {
         genres.push(data.genres[i]);
       }
-      console.log(genres);
     });
 })();
 function moviedb(link) {
@@ -29,11 +27,11 @@ function moviedb(link) {
     .then((data) => {
       for (let k = 0; k < data.results.length; k++) {
         if (data.results[k].poster_path === null) {
-          starimg(Math.round(data.results[k].vote_average));
+          starimg(data.results[k].vote_average);
           const div = `<div class="conteiner"><div class="IMG"><h1>No Poster</h1></div><div class="infodiv"><div class="infodiv"><h1>${data.results[k].original_title}</h1><span class="genre">${"moviegenre"}</span>${img}<div class="enddiv"> <span class="date">${data.results[k].release_date.slice(0, 4)}</span><span>${data.results[k].vote_average}</span> </div> </div></div>`;
           main.insertAdjacentHTML("beforeend", div);
         } else {
-          starimg(Math.round(data.results[k].vote_average));
+          starimg(data.results[k].vote_average);
           const div = `<div class="conteiner"><img src="http://image.tmdb.org/t/p/w500/${data.results[k].poster_path}"><div class="infodiv"><div class="infodiv"><h1>${data.results[k].original_title}</h1><span class="genre">${"moviegenre"}</span><div class="stars">${img}</div> <div class="enddiv"> <span class="date">${data.results[k].release_date.slice(0, 4)}</span><span>${data.results[k].vote_average}</span> </div> </div></div>`;
           main.insertAdjacentHTML("beforeend", div);
         }
@@ -42,8 +40,8 @@ function moviedb(link) {
   main.innerHTML = "";
 }
 function starimg(number) {
-  img = "";
-  for (let i = 0; i < number; i++) {
+  let img;
+  for (let i = 0; i < Math.round(number); i++) {
     img += `<img class="star" src="./images/star.svg" alt="star" />`;
   }
 }

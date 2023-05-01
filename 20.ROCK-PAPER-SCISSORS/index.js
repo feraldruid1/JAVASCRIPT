06bc3f8ef6dc7d180fff554254ad0a770039c959
nikoloz kakabadze <nk360dk@gmail.com>
2023-05-01 09:30:08 -0700

    push

---
+++ 20.ROCK-PAPER-SCISSORS/index.js
@@ -0,0 +1,26 @@
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const h1 = document.querySelector(".winner");
+const player = document.querySelector(".playercount");
+const computer = document.querySelector(".computercount");
+let playercount = 0;
+let computercount = 0;
+function game() {
+  const random = Math.floor(Math.random() * 4);
+  if (random > 2) {
+    h1.innerHTML = "you lose";
+    computercount++;
+    player.innerHTML = `player:${playercount}`;
+    computer.innerHTML = `computer:${computercount}`;
+  }
+  if (random < 2) {
+    h1.innerHTML = "you win";
+    playercount++;
+    player.innerHTML = `player:${playercount}`;
+    computer.innerHTML = `computer:${computercount}`;
+  }
+}
+rock.addEventListener("click", game);
+paper.addEventListener("click", game);
+scissors.addEventListener("click", game);

b047136213410bf3f069165e2a22df8e6af01951
nikoloz kakabadze <nk360dk@gmail.com>
2023-05-03 04:22:44 -0700

    push

---
+++ 21.TIC-TAC-TOE/index.js
@@ -0,0 +1,12 @@
+const div1 = document.querySelector(".one");
+const div2 = document.querySelector(".two");
+const div3 = document.querySelector(".three");
+const div4 = document.querySelector(".four");
+const div5 = document.querySelector(".five");
+const div6 = document.querySelector(".six");
+const div7 = document.querySelector(".seven");
+const div8 = document.querySelector(".eight");
+const div9 = document.querySelector(".nine");
+alldiv.addEventListener("click", function x() {
+  div1.innerHTML = "X";
+});

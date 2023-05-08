45ed35c818e72a2e0dad7094a1bfac4ef63cb8dd
nikoloz kakabadze <nk360dk@gmail.com>
2023-05-08 18:50:25 +0400

    push

--- 21.TIC-TAC-TOE/index.js
+++ 21.TIC-TAC-TOE/index.js
@@ -9,11 +9,38 @@ const div7 = document.querySelector(".seven");
 const div8 = document.querySelector(".eight");
 const div9 = document.querySelector(".nine");
 const main = document.querySelector("main");
+const array = [div1, div2, div3, div4, div5, div6, div7, div8, div9];
 for (let i = 0; i < alldiv.length; i++) {
   alldiv[i].addEventListener("click", function (event) {
     if (alldiv[i].innerHTML != "X" && alldiv[i].innerHTML != "O") {
       event.target.innerHTML = "X";
-      alldiv[i + 1].innerHTML = "O";
+    } else {
+      return null;
     }
+      array.forEach((elem) => {
+        if (elem.innerHTML != "X" && elem.innerHTML != "O") {
+          elem.innerHTML = "O";
+          break;
+        }
+      });
+    })
+    // if (alldiv[i - 1].innerHTML === "X" && alldiv[i + 1].innerHTML === "X") {
+    //   console.log("win");
+    // }
+    // if (alldiv[i - 1].innerHTML === "X" && alldiv[i - 2].innerHTML === "X") {
+    //   console.log("win");
+    // }
+    // if (alldiv[i - 3].innerHTML === "X" && alldiv[i + 3].innerHTML === "X") {
+    //   console.log("win");
+    // }
+    // if (alldiv[i - 3].innerHTML === "X" && alldiv[i - 6].innerHTML === "X") {
+    //   console.log("win");
+    // }
+    // if (alldiv[i - 4].innerHTML === "X" && alldiv[i + 4].innerHTML === "X") {
+    //   console.log("win");
+    // }
+    // if (alldiv[i - 2].innerHTML === "X" && alldiv[i + 2].innerHTML === "X") {
+    //   console.log("win");
+    // }
   });
 }
